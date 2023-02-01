917709ff37251843dee36016c39ac65eff84e88d
nouchata <nouchata@student.42.fr>
2023-02-01 01:37:05 +0100

    ci: action deploy to netlify

--- next.config.js
+++ next.config.js
@@ -9,7 +9,9 @@ module.exports = withBundleAnalyzer({
   },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: `/${process.env.EXPORT_BASEPATH}` || '',
+  basePath: process.env.EXPORT_BASEPATH
+    ? `/${process.env.EXPORT_BASEPATH}`
+    : '',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
